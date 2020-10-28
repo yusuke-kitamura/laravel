82835c023b9dda215a09992fcb3889f3d4268e5f
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-10-28 12:18:20 +0900

    add models and settings

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = [
+    	'body',
+    ];
+
+    public function post(){
+    	$this->belongsTo('App\Post');
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+    	'title',
+    	'body',
+    ];
+
+    public function comments(){
+    	return $this->hasMany('App\comment');
+    }
 }
