8812133d9cf6164e3dc0e6fb7c8f4a1a40bf3f40
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-10-28 15:42:53 +0900

    to show post detail

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,15 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Http\Requests\PostRequest;
 
 class PostsController extends Controller
 {
 	/**
 	*投稿等一覧を表示
 	*@return view
+	*@param int $id
 	*/
 	public function index(){
 		$posts = Post::all();
 		return view('posts.index', ['posts' => $posts]);
 	}
+
+	public function show($id){
+		$post = Post::find($id);
+		return view('posts.show',['post' => $post]);
+	}
+
+	/**
+	*投稿の作成
+	*@return view
+	*/
+	public function create(){
+		return view('posts.create');
+	}
+
+	public function store(Request $request){
+		$inputs = $request->all();
+		Post::create($inputs);
+		return redirect(route('posts.index'));
+	}
+
+
 }
