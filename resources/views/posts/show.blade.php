c450024ba7b48801444b7593536b39fc5d138245
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-10-28 18:23:10 +0900

    add comment form

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,6 +1,34 @@
 @extends('layout')
 
 @section('content')
+<form class="mb-4" method="POST" action="{{route('comments.store')}}">
+@csrf
+    <input name="post_id" type="hidden" value="{{ $post->id }}" >
+    <div class="form-group">
+        <label for="body">
+            本文
+        </label>
+
+        <textarea
+            id="body"
+            name="body"
+            class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"
+            rows="4"
+        >{{ old('body') }}</textarea>
+        @if ($errors->has('body'))
+            <div class="invalid-feedback">
+                {{ $errors->first('body') }}
+            </div>
+        @endif
+    </div>
+
+    <div class="mt-4">
+        <button type="submit" class="btn btn-primary">
+            コメントする
+        </button>
+    </div>
+</form>
+
     <div class="container mt-4">
         <div class="border p-4">
             <h1 class="h5 mb-4">
